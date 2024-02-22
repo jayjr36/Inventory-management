a5e1b98538881dd89cd1cc9e926af872ab18673e
johnjr <36johnjr@gmail.com>
2024-02-22 23:47:42 +0300

    first commit

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\UserRole;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run() {
+        UserRole::create(['name' => 'admin']);
+        UserRole::create(['name' => 'user']);
+    }
+    
+}
